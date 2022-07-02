07f2b6ce6143595553643aea000d6ca719dfc268
Nibbler <kwimbish@hotmail.com>
2022-07-02 11:12:35 -0500

    continued file set up and completed readme.md

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Social-Network-API
+
+## Description
+
+A social network web application (API). Users can share their thoughts/comments, react to friends, and make a fave friend list.
+
+## Table of Contents
+
+- [Description](#description)
+- [Technology](#Technology)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+## Technology
+
+Tech used 1. Javascript 2. Node.js 3. Express.js 4. MongoDB 5. Mongoose
+
+## Installation
+
+To run, first install: npm install => Next, start in the terminal: npm start
+
+## Usage
+
+<-- INSERT LINK HERE -->
+
+## License
+
+MIT
+
+## Contributing
+
+Me, Myself, and I. Research via MDN and our all time fave Google. Tutors Eric and Chris.
+
+## Questions
+
+kwimbish@hotmail.com or https://github.com/kwimbish2022
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const {  } = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/userdb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set('debug', true);
+
+//api's for users
+// get to get all the users
+
+// get to get a single user, by it's id; while also bringing the thoughts and friend data from that user
+
+// post a new user
+
+//put to update user by id
+
+//delete user by id (extra- also remove their thoughts when delete)
+
+//friend api's
+// post to add friend to a user friend list
+
+//delete to remove friend from user friend list
+
+//api's for thoughts
+//get to get all thoughts
+
+//get to get a single thought by id
+
+//post to make new thought (push new thought's id to the user's thoughts array field)
+
+//put to update thought by id
+
+//delete a thought by id
+
+//reactions api's
+
+// post tp create reaction (stored in  a single thought's reactions array field)
+
+// delete to pull/remove a reaction by the reaction id value
