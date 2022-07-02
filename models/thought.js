82b065411ce5a4c900884d71b53d315cc8d47830
Nibbler <kwimbish@hotmail.com>
2022-07-02 12:04:03 -0500

    models thought

--- models/thought.js
+++ models/thought.js
@@ -1,4 +1,4 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model, Types, trusted } = require('mongoose');
 const moment = require('moment');
 
 const ReactionSchema = new Schema({
@@ -30,3 +30,37 @@ const ReactionSchema = new Schema({
     }
 );
 
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 200
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', ThoughtSchema);
+
+module.exports = Thought;
